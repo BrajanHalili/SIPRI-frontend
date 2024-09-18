ee9d5e24e4b78ca6616f727d142c8541aafb7c2d
Brajan Halili <halilibrajan@gmail.com>
2024-09-18 16:31:51 -0400

    Add map interaction
    Redo the USMap to add a clickable layer for the preparation of adding a table with the data based on the country clicked.

--- src/components/USMap.jsx
+++ src/components/USMap.jsx
@@ -7,16 +7,35 @@ import 'leaflet/dist/leaflet.css';
 
 const USMap = () => {
     const [geoData, setGeoData] = useState(null);
-
+    const [country, setCountry] = useState(null);
+    const geoJsonData = {
+      "type": "FeatureCollection",
+      "features": [
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Polygon",
+            "coordinates": [[[102.0, 0.0], [103.0, 1.0], [104.0, 0.0], [102.0, 0.0]]]
+          },
+          "properties": {
+            "name": "Test Country",
+            "country_code": "TC"
+          }
+        }
+      ]
+    };
     useEffect(() => {
         async function fetchGeoData() {
             try {
             const response = await fetch('http://localhost:3006/USA/');
+            console.log(response);  // Inspect the entire response object
             if(!response.ok) {
                 console.error('Server error:', response.status, response.statusText);
                 return;
             }
-            setGeoData(response);
+            const data = await response.json();
+            setGeoData(data);
+            console.log(geoData);
             } catch (error) {
             console.error('Error fetching the GeoJSON data', error);
           }
@@ -27,16 +46,33 @@ const USMap = () => {
   // Set the initial position and zoom level
   const position = [51.505, -0.09]; // Example coordinates for the map center
 
-  return (
-    <MapContainer center={position} zoom={2} style={{ height: '500px', width: '100%' }}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
+  const onEachCountry = (country, layer) => {
+      layer.on({
+        click: () => {
+          setCountry(country.properties);
+          console.log('Country clicked: ', country.properties);
+        }
+      });
+  };
 
-      {/* Render GeoJSON data if available */}
-      {<GeoJSON data={geoData} />}
-    </MapContainer>
+  return (
+    <div>
+      <div>
+          <MapContainer center={position} zoom={2} style={{ height: '500px', width: '50%' }}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+          {geoData && (
+          <GeoJSON data={geoData} onEachFeature={onEachCountry} />
+)}
+          </MapContainer>
+      </div>
+      <div>
+
+      </div>
+    </div>
+    
   );
 };
 
